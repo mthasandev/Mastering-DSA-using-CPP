29c5397e224c4692d79c0e79819954f501740e6d
mthasandev <mthasan.dev@gmail.com>
2023-09-10 23:24:31 +0600

    new file:   04. Array ADT/ArrayADT.cpp

---
+++ 04. Array ADT/ArrayADT.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+
+using namespace std;
+
+class Array{
+    int *A;
+    int size;
+    int length;
+    public:
+     void display(){
+
+     }
+     void add(int value){
+
+     }
+     void insert(int value, int index){
+
+     }
+     void deleteElement(int index){
+
+     }
+     int search(int value){
+        return value;
+     }
+
+     int get(int index)
+}
+
+
+int main(){
+    return 0;
+}
